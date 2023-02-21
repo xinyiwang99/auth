349852dfbb1518fb928bfb4fa6dafe1a40dab2e0
xinyiwang99 <xinyiwang2022@u.northwestern.edu>
2023-02-21 02:24:42 +0000

    2

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,17 @@ class SessionsController < ApplicationController
     end
 
     def create
-        redirect_to "/sessions/new"
+        @user = User.find_by({"email" => params["email"]})
+        #if user's password matches submitted password
+        # send to companies
+        if BCrypt::Password.new(@user["password"]) == params["password"]
+            session["user_logged_in"]= @user["id"]
+            flash["notice"] = "You log in !"
+            redirect_to "/companies"
+        #otherwise send to log in page
+        else
+            flash["notice"] = "Nope"
+            redirect_to "/sessions/new"
+        end
     end
 end
--- scripts/encryption.rb
+++ scripts/encryption.rb
@@ -4,5 +4,15 @@
 
 # TODO:
 # 1. encrypt a secret and puts encrypted string
+secret = "tacos"
+puts secret
+encrypted_string= BCrypt::Password.create(secret)
+puts encrypted_string
 # 2. prepare encrypted string for testing
+user_entered_secret = "tacos"
+result = BCrypt::Password.create(secret) == user_entered_secret
+puts result
 # 3. test secret against prepared encrypted string
+user_entered_secret = "puppies"
+result = BCrypt::Password.create(secret) == user_entered_secret
+puts result
